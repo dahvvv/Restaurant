a211ca29ea2961eb1e1fe76de2e361cf91297e37
dahvvv <davidjosephkoenig@gmail.com>
2014-10-16 12:36:35 -0400

    haz price method for order class!!

--- models/order.rb
+++ models/order.rb
@@ -1,5 +1,14 @@
 class Order < ActiveRecord::Base
   belongs_to(:party)
   belongs_to(:food)
+
+  def charge
+    food = Food.where(:id => self.food_id)[0]
+    if self.no_charge == true
+      0
+    else
+      food.price.to_s[0..-3] + "." + food.price.to_s[-2..-1]
+    end
+  end
 end
 
