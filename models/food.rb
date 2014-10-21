25af3a913223e4ceca25493a77a20aac0257196b
dahvvv <davidjosephkoenig@gmail.com>
2014-10-20 23:29:05 -0400

    refactored with food_names_for_receipt method

--- models/food.rb
+++ models/food.rb
@@ -4,18 +4,9 @@ class Food < ActiveRecord::Base
   has_many(:orders)
   has_many(:parties, :through => :orders)
 
-  # def price_to_int
-  #   # (self.price.to_f * 100).to_i
-  #   self.update(:price => self.dollars.to_s + self.cents.to_s)
-  # end
-
   def priceprint
     self.price.to_s[0..-3] + "." + self.price.to_s[-2..-1]
-    # self.update(:price => :dollars.to_s + :cents.to_s)
   end
 
-  # def order_id
-  #   Order.where(:food_id => self.id).id
-  # end
 end
 
--- models/party.rb
+++ models/party.rb
@@ -1,4 +1,11 @@
 class Party < ActiveRecord::Base
   has_many(:orders)
   has_many(:foods, :through => :orders)
+
+  def food_names_for_receipt
+    self.orders.map do |order|
+      order.food_name.gsub(" ", "_").to_sym
+    end
+  end
+
 end
